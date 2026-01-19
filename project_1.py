2b62ba1168fb987a96fdfd6fe91a74bde14cdf49
MariyaSilviya <inbox2silviya@gmail.com>
2026-01-19 12:07:51 +0530

    added comments

--- project_1.py
+++ project_1.py
@@ -1,3 +1,10 @@
+# At the start of the day you have a checklist The tasks which you were able to finish, should get added to completed_tasks
+#  The tasks which you were not able to finish, should get added to incomplete_tasks 
+# This project is about organizing your daily tasks into two categories: completed tasks and incomplete tasks.
+#  Here’s how it works: 1. At the start of the day, you create a checklist of tasks you want to accomplish.
+#  2. At the end of the day, you review which tasks you could finish and which you couldn’t. – If a task is finished, you move it to the completed tasks list. – If a task is not finished, you move it to the incomplete tasks list.
+#  This way, by the end of each day, you’ll have a clear overview of what you completed and what still needs attention.
+
 checklists=[]
 completed_tasks=[]
 incompleted_tasks=[]
